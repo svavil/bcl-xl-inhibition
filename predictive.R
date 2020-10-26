3340d909903a7f32cbcc484e9d95665e17480637
svavil <svavil@gmail.com>
2020-10-26 23:38:43 +0300

    Added a random forest predictive model

--- predictive.R
+++ predictive.R
@@ -1,10 +1,11 @@
 # A script to create predictive model for Bcl-xL inhibition
 # Created by: svavil
 # Created on: 2020-10-24
-# Edited on: 2020-10-25
+# Edited on: 2020-10-26
 
 library(tidyverse)
 library(readxl)
+library(randomForest)
 theme_set(theme_bw(base_size = 16))
 theme_update(panel.grid.minor = element_blank())
 data_block_1 <- read_xlsx("bcl-xl_1_descriptors.xlsx") %>% 
@@ -42,8 +43,18 @@ ggplot(data = data_block_1) +
 predictors <- data_block_1 %>%
   select(-PUBCHEM_ACTIVITY_SCORE, -PUBCHEM_SID, -PUBCHEM_ACTIVITY_OUTCOME, -IC, -SMILES, -Norm_Act, -PUBCHEM_CID)
 
-model <- lm(data_block_1$IC~., data = predictors)
-predicted.IC <- predict.lm(model, newdata = predictors)
+model.regression <- lm(data_block_1$IC~., data = predictors)
+predicted.IC <- predict.lm(model.regression, newdata = predictors)
 ggplot() + 
   geom_point(aes(x = data_block_1$IC, y = predicted.IC)) + 
+  geom_line(aes(x = data_block_1$IC, y = data_block_1$IC), color = "red", size = 1)
+
+model.class <- glm(data_block_1$PUBCHEM_ACTIVITY_OUTCOME %>% recode("Inactive" = 0, "Active" = 1) ~ ., data = predictors, family = "binomial")
+predicted.outcome <- predict.glm(model.class, newdata = predictors, family = "binomial") > 0
+table(predicted.outcome, data_block_1$PUBCHEM_ACTIVITY_OUTCOME)
+
+rf.model <- randomForest(data_block_1$IC ~ ., data = predictors, ntree = 1000)
+rf.predictions <- predict(rf.model, newdata = predictors)
+ggplot() + 
+  geom_point(aes(x = data_block_1$IC, y = rf.predictions)) + 
   geom_line(aes(x = data_block_1$IC, y = data_block_1$IC), color = "red", size = 1)
